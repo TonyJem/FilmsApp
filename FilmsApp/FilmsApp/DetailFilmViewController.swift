6cac643089c12fbbd4b9e491da90b5a05b65019c
TonyJem <a.jemanov@gmail.com>
2021-08-17 17:48:50 +0300

    Create DetailFilmViewController

--- FilmsApp/FilmsApp/DetailFilmViewController.swift
+++ FilmsApp/FilmsApp/DetailFilmViewController.swift
@@ -1,10 +1,10 @@
 import UIKit
 
-class ViewController: UIViewController {
-
+class DetailFilmViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
 }
