30e48ec9a03757aacd1cf12d97e9a982ec9894b2
TonyJem <a.jemanov@gmail.com>
2021-08-17 18:02:45 +0300

    Create JSONModel class

---
+++ FilmsApp/FilmsApp/JSONModel.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+class JSONModel {
+    var original_title: String?
+    var poster_path: String?
+    var release_date: String?
+    var overview: String?
+    var vote_average: Double?
+    var backdrop_path: String?
+}
