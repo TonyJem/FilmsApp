e87f25deb18699ff73b982e19125feb937629ca7
TonyJem <a.jemanov@gmail.com>
2021-08-25 22:17:25 +0300

    Add protocols to MainViewController

--- FilmsApp/FilmsApp/MainViewController.swift
+++ FilmsApp/FilmsApp/MainViewController.swift
@@ -7,7 +7,37 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        filmsSearchBar.delegate = self
+        
+        mainCollectionView.delegate = self
+        mainCollectionView.dataSource = self
+        
+        mainCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellID")
     }
+    
+}
 
+// MARK: UISearchBarDelegate
+extension MainViewController: UISearchBarDelegate {
+    
+}
+
+// MARK: UICollectionViewDelegate
+extension MainViewController: UICollectionViewDelegate {
+    
+}
+
+// MARK: UICollectionViewDataSource
+extension MainViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath) as? UICollectionViewCell
+//        cell.backgroundColor = indexPath.item % 2 == 0 ? .red : .blue
+        return UICollectionViewCell()
+    }
 }
