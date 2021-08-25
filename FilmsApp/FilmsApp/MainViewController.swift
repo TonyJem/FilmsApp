47176c7ea3b7536de1a59fd120a816db28e7103f
TonyJem <a.jemanov@gmail.com>
2021-08-25 22:04:20 +0300

    Create outlets for MainViewController

--- FilmsApp/FilmsApp/MainViewController.swift
+++ FilmsApp/FilmsApp/MainViewController.swift
@@ -1,7 +1,10 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet private weak var filmsSearchBar: UISearchBar!
+    @IBOutlet private weak var mainCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
