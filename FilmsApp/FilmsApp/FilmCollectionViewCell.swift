0db5cf70fbaf3800376f71ba2ae36534007046fb
TonyJem <a.jemanov@gmail.com>
2021-08-26 08:02:05 +0300

    Add outlets in FilmCollectionViewCell

--- FilmsApp/FilmsApp/FilmCollectionViewCell.swift
+++ FilmsApp/FilmsApp/FilmCollectionViewCell.swift
@@ -2,4 +2,10 @@ import UIKit
 
 class FilmCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet private weak var containerView: UIView!
+    @IBOutlet private weak var posterImageView: UIImageView!
+    @IBOutlet private weak var filmTitleLabel: UILabel!
+    @IBOutlet private weak var yearLabel: UILabel!
+    @IBOutlet private weak var ratingLabel: UILabel!
+    
 }
