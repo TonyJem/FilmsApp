7b552acfd5e1475d796ce0e40b0036dbee842d79
TonyJem <a.jemanov@gmail.com>
2022-03-19 20:49:52 +0200

    Add outlets to DetailFilmViewController

--- FilmsApp/FilmsApp/ViewControllers/DetailFilmViewController.swift
+++ FilmsApp/FilmsApp/ViewControllers/DetailFilmViewController.swift
@@ -2,8 +2,14 @@ import UIKit
 
 class DetailFilmViewController: UIViewController {
     
+    @IBOutlet private weak var posterImageView: UIImageView!
+    @IBOutlet private weak var filmTitleLabel: UILabel!
+    @IBOutlet private weak var releaseYearLabel: UILabel!
+    @IBOutlet private weak var ratingLabel: UILabel!
+    @IBOutlet private weak var galleryCollection: UICollectionView!
+    @IBOutlet private weak var descriptionTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 }
