8b0e4f1a2988e2db0aeeebabfc06516a08af3fef
TonyJem <a.jemanov@gmail.com>
2022-03-20 07:43:19 +0200

    Create PosterFullViewController

---
+++ FilmsApp/FilmsApp/ViewControllers/PosterFullViewController.swift
@@ -0,0 +1,11 @@
+import Foundation
+import UIKit
+
+final class PosterFullViewController: UIViewController {
+    
+    @IBOutlet private weak var fullPosterImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
