0fa984c7d18f5ffa25d7ceb89f835084fcd8b56f
TonyJem <a.jemanov@gmail.com>
2022-03-20 08:34:57 +0200

    Add closeButton to PosterFullViewController

--- FilmsApp/FilmsApp/ViewControllers/PosterFullViewController.swift
+++ FilmsApp/FilmsApp/ViewControllers/PosterFullViewController.swift
@@ -3,9 +3,29 @@ import UIKit
 
 final class PosterFullViewController: UIViewController {
     
+    @IBOutlet private weak var closeButton: UIButton!
     @IBOutlet private weak var fullPosterImageView: UIImageView!
     
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupViews()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        closeButton.layer.cornerRadius = closeButton.frame.width / 2
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func closeButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - Private methods
+    
+    private func setupViews() {
+        closeButton.clipsToBounds = true
     }
 }
