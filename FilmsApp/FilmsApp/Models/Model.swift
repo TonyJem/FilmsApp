419308d02f9477465512d0a75e9662fb8f92c412
TonyJem <a.jemanov@gmail.com>
2022-03-22 07:36:41 +0200

    Update Model.swift

--- FilmsApp/FilmsApp/Models/Model.swift
+++ FilmsApp/FilmsApp/Models/Model.swift
@@ -5,11 +5,11 @@ class Item {
     var id: Int?
     var testPic: String?
     var testTitle: String?
-    var testYear: String?
-    var testRating: String?
+    var testYear: Int?
+    var testRating: Double?
     var isLiked: Bool
     
-    init(id: Int? = nil, testPic: String?, testTitle: String?, testYear: String?, testRating: String?, isliked: Bool = false) {
+    init(id: Int? = nil, testPic: String?, testTitle: String?, testYear: Int?, testRating: Double?, isliked: Bool = false) {
         self.id = id
         self.testPic = testPic
         self.testTitle = testTitle
@@ -21,21 +21,21 @@ class Item {
 
 class Model {
     var testArray: [Item] = [
-        Item(id: 0, testPic: "image01", testTitle: "Film 01", testYear: "2001", testRating: "4.1", isliked: true),
-        Item(id: 1, testPic: "image02", testTitle: "Film 02", testYear: "2002", testRating: "4.2", isliked: false),
-        Item(id: 2, testPic: "image03", testTitle: "Film 03", testYear: "2003", testRating: "4.3", isliked: true),
-        Item(id: 3, testPic: "image04", testTitle: "Film 04", testYear: "2004", testRating: "4.4", isliked: true),
-        Item(id: 4, testPic: "image05", testTitle: "Film 05", testYear: "2005", testRating: "4.5", isliked: false),
-        Item(id: 5, testPic: "image06", testTitle: "Film 06", testYear: "2006", testRating: "4.6", isliked: true),
-        Item(id: 6, testPic: "image07", testTitle: "Film 07", testYear: "2007", testRating: "4.7", isliked: true),
-        Item(id: 7, testPic: "image08", testTitle: "Film 08", testYear: "2008", testRating: "4.8", isliked: false),
-        Item(id: 8, testPic: "image09", testTitle: "Film 09", testYear: "2009", testRating: "4.9", isliked: true),
-        Item(id: 9, testPic: "image10", testTitle: "Film 10", testYear: "2010", testRating: "5.0", isliked: false),
-        Item(id: 10, testPic: "image11", testTitle: "Film 11", testYear: "2011", testRating: "5.1", isliked: true),
-        Item(id: 11, testPic: "image12", testTitle: "Film 12", testYear: "2012", testRating: "5.2", isliked: true),
-        Item(id: 12, testPic: "image13", testTitle: "Film 13", testYear: "2013", testRating: "5.3", isliked: false),
-        Item(id: 13, testPic: "image14", testTitle: "Film 14", testYear: "2014", testRating: "5.4", isliked: false),
-        Item(id: 14, testPic: "image15", testTitle: "Film 15", testYear: "2015", testRating: "5.5", isliked: true)
+        Item(id: 0, testPic: "image01", testTitle: "Film 01", testYear: 2001, testRating: 4.1, isliked: true),
+        Item(id: 1, testPic: "image02", testTitle: "Film 02", testYear: 2002, testRating: 4.2, isliked: false),
+        Item(id: 2, testPic: "image03", testTitle: "Film 03", testYear: 2003, testRating: 4.3, isliked: true),
+        Item(id: 3, testPic: "image04", testTitle: "Film 04", testYear: 2004, testRating: 4.4, isliked: true),
+        Item(id: 4, testPic: "image05", testTitle: "Film 05", testYear: 2005, testRating: 4.5, isliked: false),
+        Item(id: 5, testPic: "image06", testTitle: "Film 06", testYear: 2006, testRating: 4.6, isliked: true),
+        Item(id: 6, testPic: "image07", testTitle: "Film 07", testYear: 2007, testRating: 4.7, isliked: true),
+        Item(id: 7, testPic: "image08", testTitle: "Film 08", testYear: 2008, testRating: 4.8, isliked: false),
+        Item(id: 8, testPic: "image09", testTitle: "Film 09", testYear: 2009, testRating: 4.9, isliked: true),
+        Item(id: 9, testPic: "image10", testTitle: "Film 10", testYear: 2010, testRating: 5.0, isliked: false),
+        Item(id: 10, testPic: "image11", testTitle: "Film 11", testYear: 2011, testRating: 5.1, isliked: true),
+        Item(id: 11, testPic: "image12", testTitle: "Film 12", testYear: 2012, testRating: 5.2, isliked: true),
+        Item(id: 12, testPic: "image13", testTitle: "Film 13", testYear: 2013, testRating: 5.3, isliked: false),
+        Item(id: 13, testPic: "image14", testTitle: "Film 14", testYear: 2014, testRating: 5.4, isliked: false),
+        Item(id: 14, testPic: "image15", testTitle: "Film 15", testYear: 2015, testRating: 5.5, isliked: true)
     ]
     
     var newTestArray: [Item] = []
@@ -89,8 +89,8 @@ class Model {
         
         cell.posterImageView.image = UIImage(named: newTestArray[indexPath.row].testPic ?? "image01")
         cell.filmTitleLabel.text = newTestArray[indexPath.row].testTitle
-        cell.yearLabel.text = newTestArray[indexPath.row].testYear
-        cell.ratingLabel.text = newTestArray[indexPath.row].testRating
+        cell.yearLabel.text = String(newTestArray[indexPath.row].testYear ?? 0)
+        cell.ratingLabel.text = String(newTestArray[indexPath.row].testRating ?? 0.0)
         
         return cell
     }
