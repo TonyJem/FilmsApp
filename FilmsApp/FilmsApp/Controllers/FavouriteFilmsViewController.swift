2babafb5d9201c7343f01d0f79fd4a665be677c0
TonyJem <a.jemanov@gmail.com>
2022-03-23 19:52:06 +0200

    Update FavoriteFilmsViewController

--- FilmsApp/FilmsApp/Controllers/FavouriteFilmsViewController.swift
+++ FilmsApp/FilmsApp/Controllers/FavouriteFilmsViewController.swift
@@ -1,10 +1,53 @@
 import UIKit
 
-class FavouriteFilmsViewController: UIViewController {
+class FavoriteFilmsViewController: UIViewController {
+    
+    @IBOutlet weak var favoriteFilmsCollectionView: UICollectionView!
+    
+    let model = Model()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        model.showLikedFilms()
+        
+        favoriteFilmsCollectionView.delegate = self
+        favoriteFilmsCollectionView.dataSource = self
+        
+        let xibFavCell = UINib(nibName: "FavoriteFilmViewCell", bundle: nil)
+        
+        favoriteFilmsCollectionView.register(xibFavCell, forCellWithReuseIdentifier: "FavoriteFilmCell")
+        
+        favoriteFilmsCollectionView.reloadData()
+    }
+    
+}
+
+extension FavoriteFilmsViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return model.likedTestArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = favoriteFilmsCollectionView.dequeueReusableCell(withReuseIdentifier: "FavoriteFilmCell", for: indexPath) as? FavoriteFilmViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.data = self.model.likedTestArray[indexPath.item]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        guard let destinationVC = storyboard?.instantiateViewController(withIdentifier: "DetailFilmViewControllerS") as? DetailFilmViewController else {
+            return
+        }
+        
+        destinationVC.receivedIndex = model.likedTestArray[indexPath.row].id ?? 0
+        navigationController?.pushViewController(destinationVC, animated: true)
     }
     
 }
