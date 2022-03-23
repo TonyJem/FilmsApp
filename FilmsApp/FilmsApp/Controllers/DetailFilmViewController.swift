fea87b0cce5ee718ddafc0555327bd8bad8adf53
TonyJem <a.jemanov@gmail.com>
2022-03-23 18:10:06 +0200

    Update DetailFilmViewController

--- FilmsApp/FilmsApp/Controllers/DetailFilmViewController.swift
+++ FilmsApp/FilmsApp/Controllers/DetailFilmViewController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class DetailFilmViewController: UIViewController {
     
+    @IBOutlet weak var likeButton: UIButton!
     @IBOutlet private weak var posterImageView: UIImageView!
     @IBOutlet private weak var filmTitleLabel: UILabel!
     @IBOutlet private weak var releaseYearLabel: UILabel!
@@ -9,18 +10,31 @@ class DetailFilmViewController: UIViewController {
     @IBOutlet private weak var galleryCollection: UICollectionView!
     @IBOutlet private weak var descriptionTextView: UITextView!
     
+    var cameFromFav: Bool = Bool()
     var receivedIndex: Int = Int()
     var transition: RoundingTransition = RoundingTransition()
     var model = Model()
     
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if model.testArray[receivedIndex].isLiked == true {
+            likeButton.alpha = 1
+            likeButton.tintColor = .black
+        } else {
+            likeButton.alpha = 0.45
+            likeButton.tintColor = .gray
+        }
         
         posterImageView.image = UIImage(named: model.testArray[receivedIndex].testPic ?? "image01")
         filmTitleLabel.text = model.testArray[receivedIndex].testTitle
         releaseYearLabel.text = String(model.testArray[receivedIndex].testYear ?? 0)
         ratingLabel.text = String(model.testArray[receivedIndex].testRating ?? 0.0)
     }
+    
+    // MARK: - Override methods
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let destinationVC = segue.destination as? PosterFullViewController else { return }
@@ -30,9 +44,17 @@ class DetailFilmViewController: UIViewController {
         destinationVC.modalPresentationStyle = .custom
     }
     
+    // MARK: - Actions
+    
+    @IBAction func likeButtonPressed(_ sender: UIButton) {
+        print("🟢 likeButtonPressed in DetailFilmViewController")
+    }
+    
     @IBAction func tapGestureAction(_ sender: Any) {
     }
 }
+
+// MARK: - UIViewControllerTransitioningDelegate
 
 extension DetailFilmViewController: UIViewControllerTransitioningDelegate {
     func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
