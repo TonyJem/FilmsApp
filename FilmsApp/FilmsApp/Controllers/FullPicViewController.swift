e6cb12fffd344cbc367c33cc451fdb82c4a0792d
TonyJem <a.jemanov@gmail.com>
2022-03-26 17:19:03 +0200

    Setup FullPicViewController and storyboard for it

--- FilmsApp/FilmsApp/Controllers/FullPicViewController.swift
+++ FilmsApp/FilmsApp/Controllers/FullPicViewController.swift
@@ -1,10 +1,22 @@
 import UIKit
 
-class FullPicViewController: UIViewController {
+class FullPicViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var fullPicGalleryCollection: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        fullPicGalleryCollection.dataSource = self
+        fullPicGalleryCollection.delegate = self
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = fullPicGalleryCollection.dequeueReusableCell(withReuseIdentifier: "FullPicCell", for: indexPath)
+        return cell
+    }
 }
