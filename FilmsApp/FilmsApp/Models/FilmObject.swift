10d8271f8aaab8ed45dcc0971ef066f422a894c8
TonyJem <a.jemanov@gmail.com>
2022-03-26 17:53:37 +0200

    Create FilmObject Realm model file

---
+++ FilmsApp/FilmsApp/Models/FilmObject.swift
@@ -0,0 +1,14 @@
+import RealmSwift
+
+class FilmObject: Object {
+    @Persisted var id: Int = 0
+    @Persisted var filmPic: String = ""
+    @Persisted var filmTitle: String = ""
+    @Persisted var releaseYear: Int = 0
+    @Persisted var filmRating: Double = 0.0
+    @Persisted var isLikedByUser: Bool = false
+    
+    override class func primaryKey() -> String? {
+        return "id"
+    }
+}
