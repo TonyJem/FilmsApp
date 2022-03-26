2e30159d1c14c11f59fbbad2a5f04432f22a8f58
TonyJem <a.jemanov@gmail.com>
2022-03-26 16:38:27 +0200

    Setup FilmPicsViewController

--- FilmsApp/FilmsApp/Controllers/FilmPicsViewController.swift
+++ FilmsApp/FilmsApp/Controllers/FilmPicsViewController.swift
@@ -1,10 +1,23 @@
 import UIKit
 
-class FilmPicsViewController: UIViewController {
+class FilmPicsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var picsNumberLabel: UILabel!
+    @IBOutlet weak var filmPicsCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        filmPicsCollectionView.dataSource = self
+        filmPicsCollectionView.delegate = self
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = filmPicsCollectionView.dequeueReusableCell(withReuseIdentifier: "FilmPicCell", for: indexPath)
+        return cell
+    }
 }
