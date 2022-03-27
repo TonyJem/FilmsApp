5aec3821fb128e3143d79a9a83f883d6a7873841
TonyJem <a.jemanov@gmail.com>
2022-03-27 17:52:50 +0300

    Change Model file to be working with Realm

--- FilmsApp/FilmsApp/Models/Model.swift
+++ FilmsApp/FilmsApp/Models/Model.swift
@@ -2,104 +2,44 @@ import Foundation
 import UIKit
 import RealmSwift
 
-class Item {
-    var id: Int?
-    var testPic: String?
-    var testTitle: String?
-    var testYear: Int?
-    var testRating: Double?
-    var isLiked: Bool
-    
-    init(id: Int?, testPic: String?, testTitle: String?, testYear: Int?, testRating: Double?, isliked: Bool) {
-        self.id = id
-        self.testPic = testPic
-        self.testTitle = testTitle
-        self.testYear = testYear
-        self.testRating = testRating
-        self.isLiked = isliked
-    }
-}
-
 class Model {
     
     let realm = try? Realm()
     
-    var filmObjects: Results<FilmObject>?
+    var filmObjects: Results<FilmObject>? {
+        return realm?.objects(FilmObject.self)
+    }
     
-    var testArray: [Item] = [
-        Item(id: 0, testPic: "image01", testTitle: "Film 01", testYear: 2001, testRating: 4.1, isliked: true),
-        Item(id: 1, testPic: "image02", testTitle: "Film 02", testYear: 2002, testRating: 4.2, isliked: false),
-        Item(id: 2, testPic: "image03", testTitle: "Film 03", testYear: 2003, testRating: 4.3, isliked: true),
-        Item(id: 3, testPic: "image04", testTitle: "Film 04", testYear: 2004, testRating: 4.4, isliked: true),
-        Item(id: 4, testPic: "image05", testTitle: "Film 05", testYear: 2005, testRating: 4.5, isliked: false),
-        Item(id: 5, testPic: "image06", testTitle: "Film 06", testYear: 2006, testRating: 4.6, isliked: true),
-        Item(id: 6, testPic: "image07", testTitle: "Film 07", testYear: 2007, testRating: 4.7, isliked: true),
-        Item(id: 7, testPic: "image08", testTitle: "Film 08", testYear: 2008, testRating: 4.8, isliked: false),
-        Item(id: 8, testPic: "image09", testTitle: "Film 09", testYear: 2009, testRating: 4.9, isliked: true),
-        Item(id: 9, testPic: "image10", testTitle: "Film 10", testYear: 2010, testRating: 5.0, isliked: false),
-        Item(id: 10, testPic: "image11", testTitle: "Film 11", testYear: 2011, testRating: 5.1, isliked: true),
-        Item(id: 11, testPic: "image12", testTitle: "Film 12", testYear: 2012, testRating: 5.2, isliked: true),
-        Item(id: 12, testPic: "image13", testTitle: "Film 13", testYear: 2013, testRating: 5.3, isliked: false),
-        Item(id: 13, testPic: "image14", testTitle: "Film 14", testYear: 2014, testRating: 5.4, isliked: false),
-        Item(id: 14, testPic: "image15", testTitle: "Film 15", testYear: 2015, testRating: 5.5, isliked: true)
-    ]
-    
-    var newTestArray: [Item] = []
-    var likedTestArray: [Item] = []
+    var likedFilmObjects: Results<FilmObject>?
+    var arrayHelper: Results<FilmObject>?
     var sortAscending: Bool = true
     
-    func readRealmData() {
-        filmObjects = realm?.objects(FilmObject.self)
-    }
-    
     func ratingSort() {
-        self.testArray.sort {
-            sortAscending ? ($0.testRating ?? 0) < ($1.testRating ?? 0) : ($0.testRating ?? 0) > ($1.testRating ?? 0)
-        }
-        newTestArray = testArray
-    }
-    
-    func showLikedFilms() {
-        for item in testArray {
-            if item.isLiked == true {
-                self.likedTestArray.append(item)
-            }
-        }
+        arrayHelper = filmObjects?.sorted(byKeyPath: "filmRating", ascending: sortAscending)
     }
     
     func search(searchTextValue: String) {
-        newTestArray = []
-        
-        if searchTextValue == "" {
-            newTestArray = testArray
-        } else {
-            for item in testArray {
-                guard let unwrItem = item.testTitle else {
-                    return
+        let predicate = NSPredicate(format: "filmTitle CONTAINS [c]%@", searchTextValue)
+        arrayHelper = filmObjects?.filter(predicate)
+    }
+    
+    func showLikedFilms() {
+        let likeFilter = NSPredicate(format: "isLikedByUser = true")
+        likedFilmObjects = filmObjects?.filter(likeFilter)
+    }
+    func updateLike(at item: Int) {
+        if let film = filmObjects?[item] {
+            do {
+                try realm?.write {
+                    film.isLikedByUser = !film.isLikedByUser
                 }
-                if unwrItem.contains(searchTextValue) {
-                    newTestArray.append(item)
-                }
+            } catch {
+                print("Error saving done status, \(error)")
             }
         }
-        newTestArray = testArray.filter({
-            $0.testTitle?.range(of: searchTextValue, options: .caseInsensitive) != nil
-        })
     }
     
-    // MARK: - CollectionViewItems Methods
-    
-    func countItems() -> Int {
-        newTestArray.count
-    }
-    
-    func buildCell(cell: FilmCollectionViewCell, indexPath: IndexPath) -> UICollectionViewCell {
-        
-        cell.posterImageView.image = UIImage(named: newTestArray[indexPath.row].testPic ?? "image01")
-        cell.filmTitleLabel.text = newTestArray[indexPath.row].testTitle
-        cell.yearLabel.text = String(newTestArray[indexPath.row].testYear ?? 0)
-        cell.ratingLabel.text = String(newTestArray[indexPath.row].testRating ?? 0.0)
-        
-        return cell
+    func changeColor(at sender: UIButton) {
+        sender.backgroundColor = .red
     }
 }
