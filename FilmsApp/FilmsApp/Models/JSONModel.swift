66ca7de231cafbd1d3abb14a517f673417c4e11e
TonyJem <a.jemanov@gmail.com>
2022-03-29 22:03:02 +0300

    Update JSONModel.swift

--- FilmsApp/FilmsApp/Models/JSONModel.swift
+++ FilmsApp/FilmsApp/Models/JSONModel.swift
@@ -1,11 +1,37 @@
 import Foundation
 
-class JSONModel {
-    var id: Int?
-    var poster_path: String?
-    var original_title: String?
-    var overview: String?
-    var release_date: String?
-    var vote_average: Double?
-    var backdrop_path: String?
+// MARK: - MovieList
+struct MovieList: Codable {
+    let page: Int
+    let results: [Result]
+    let totalPages: Int
+    let totalResults: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case page
+        case results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let id: Int
+    let posterPath: String
+    let originalTitle: String
+    let overview: String
+    let releaseDate: String
+    let voteAverage: Double
+    let backdropPath: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case posterPath = "poster_path"
+        case originalTitle = "original_title"
+        case overview
+        case releaseDate = "release_date"
+        case voteAverage = "vote_average"
+        case backdropPath = "backdrop_path"
+    }
 }
