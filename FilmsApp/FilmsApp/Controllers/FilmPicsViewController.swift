d7e11e0ab88112c25519cb5d665c95771f98b378
TonyJem <a.jemanov@gmail.com>
2022-04-15 21:49:12 +0300

    Update FilmPicsViewController.swift

--- FilmsApp/FilmsApp/Controllers/FilmPicsViewController.swift
+++ FilmsApp/FilmsApp/Controllers/FilmPicsViewController.swift
@@ -1,19 +1,26 @@
 import UIKit
 
-class FilmPicsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class FilmPicsViewController: UIViewController {
     
     @IBOutlet weak var picsNumberLabel: UILabel!
     @IBOutlet weak var filmPicsCollectionView: UICollectionView!
     
-    let model = Core.model
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupDelegates()
+    }
+    
+    // MARK: - Private methods
+    private func setupDelegates() {
         filmPicsCollectionView.dataSource = self
         filmPicsCollectionView.delegate = self
     }
     
+}
+
+// MARK: - UICollectionViewDataSource
+extension FilmPicsViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
@@ -22,4 +29,8 @@ class FilmPicsViewController: UIViewController, UICollectionViewDelegate, UIColl
         let cell = filmPicsCollectionView.dequeueReusableCell(withReuseIdentifier: "FilmPicCell", for: indexPath)
         return cell
     }
+}
+
+// MARK: - UICollectionViewDelegate
+extension FilmPicsViewController: UICollectionViewDelegate {
 }
