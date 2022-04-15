2df6e5c0920a651b84b59e720613ac57af705d54
TonyJem <a.jemanov@gmail.com>
2022-04-15 21:40:43 +0300

    Update FavouriteFilmsViewController.swift

--- FilmsApp/FilmsApp/Controllers/FavouriteFilmsViewController.swift
+++ FilmsApp/FilmsApp/Controllers/FavouriteFilmsViewController.swift
@@ -7,6 +7,7 @@ class FavoriteFilmsViewController: UIViewController {
     
     let model = Core.model
     
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -16,19 +17,24 @@ class FavoriteFilmsViewController: UIViewController {
         let xibFavCell = UINib(nibName: "FavoriteFilmViewCell", bundle: nil)
         favoriteFilmsCollectionView.register(xibFavCell, forCellWithReuseIdentifier: "FavoriteFilmCell")
         
-        DispatchQueue.main.async {
-            self.favoriteFilmsCollectionView.reloadData()
-        }
+        reloadCollectionViewData()
     }
     
+    // MARK: - Actions
     @IBAction func updateButtonPressed(_ sender: UIBarButtonItem) {
+        reloadCollectionViewData()
+    }
+    
+    // MARK: - Private Methods
+    private func reloadCollectionViewData() {
         DispatchQueue.main.async {
             self.favoriteFilmsCollectionView.reloadData()
         }
     }
 }
 
-extension FavoriteFilmsViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+// MARK: - UICollectionViewDataSource
+extension FavoriteFilmsViewController:  UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return model.likedFilms?.count ?? 0
@@ -46,7 +52,10 @@ extension FavoriteFilmsViewController: UICollectionViewDelegate, UICollectionVie
         
         return cell
     }
-    
+}
+
+// MARK: - UICollectionViewDelegate
+extension FavoriteFilmsViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
         guard let destinationVC = storyboard?.instantiateViewController(withIdentifier: "DetailFilmViewControllerS") as? DetailFilmViewController else {
