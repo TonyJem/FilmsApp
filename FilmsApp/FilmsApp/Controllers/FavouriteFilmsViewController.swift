167bd94b57f5e4c29fbcc3892839f11c05b4839d
TonyJem <a.jemanov@gmail.com>
2022-04-17 08:24:10 +0300

    Update FavouriteFilmsViewController.swift

--- FilmsApp/FilmsApp/Controllers/FavouriteFilmsViewController.swift
+++ FilmsApp/FilmsApp/Controllers/FavouriteFilmsViewController.swift
@@ -40,9 +40,7 @@ class FavoriteFilmsViewController: UIViewController {
     }
     
     private func showAlert(title: String, message: String?, completionHandler: @escaping () -> Void) {
-        
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
         let ok = UIAlertAction(title: "OK", style: .default) { _ in completionHandler() }
         let cancel = UIAlertAction(title: "Cancel", style: .cancel)
         
