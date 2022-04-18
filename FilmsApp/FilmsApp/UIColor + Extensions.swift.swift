7152c95ca72aff73f3a96a8d4f12f6886289850b
TonyJem <a.jemanov@gmail.com>
2022-04-18 10:10:01 +0300

    Create UIColor extension

---
+++ FilmsApp/FilmsApp/UIColor + Extensions.swift.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+//#colorLiteral()
+
+extension UIColor {
+    static let selectedCellBackground = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+    static let filmCellBackground = #colorLiteral(red: 0.8823529412, green: 1, blue: 0.7764705882, alpha: 1)
+    static let likedFilmCellBackground = #colorLiteral(red: 0.8470588235, green: 0.9450980392, blue: 1, alpha: 1)
+}
