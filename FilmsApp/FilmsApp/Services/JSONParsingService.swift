accd177ff8692ed0a8fe7a0c77bcbe6fc870a5a6
TonyJem <a.jemanov@gmail.com>
2022-04-18 17:46:10 +0300

    Update JSONParsingService.swift

--- FilmsApp/FilmsApp/Services/JSONParsingService.swift
+++ FilmsApp/FilmsApp/Services/JSONParsingService.swift
@@ -6,13 +6,10 @@ class JSONParsingService {
     func parseJSON(parseData: Data, parseError: Error?) {
         do {
             let filmObject = try JSONDecoder().decode(MovieList.self, from: parseData)
-            
             let jsonObjects = filmObject.results
             let realm = try? Realm()
             
-            // Write data to DB
             try realm?.write({
-                // Loop for JSON Objects array
                 for item in jsonObjects {
                     let object = FilmObject()
                     
@@ -29,17 +26,12 @@ class JSONParsingService {
                         object.about = unwrAbout
                         object.releaseYear = Int(unwrReleaseYear.prefix(4)) ?? 0000
                         object.filmRating = unwrFilmRating
-                        
-                        //object.screenshots = item.backdrop_path ?? "N/A"
-                        //object.isLikedByUser = newOne.isLikedByUser
                     }
                     realm?.add(object, update: .all)
                 }
             })
-            
         } catch let error {
             print(error)
         }
     }
-    
 }
