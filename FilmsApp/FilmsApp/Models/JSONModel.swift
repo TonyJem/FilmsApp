0d089e219b7ee31c9744e4a52edc6e403d3beb3d
TonyJem <a.jemanov@gmail.com>
2022-04-18 18:01:47 +0300

    Update JSONModel.swift

--- FilmsApp/FilmsApp/Models/JSONModel.swift
+++ FilmsApp/FilmsApp/Models/JSONModel.swift
@@ -35,3 +35,32 @@ struct Result: Codable {
         case backdropPath = "backdrop_path"
     }
 }
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let screenShots = try? newJSONDecoder().decode(ScreenShots.self, from: jsonData)
+
+// https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=<<api_key>>&language=en-US
+
+// MARK: - ScreenShots
+struct ScreenShots: Codable {
+    let id: Int
+    let backdrops: [Backdrop]
+    let posters: [Backdrop]
+}
+
+// MARK: - Backdrop
+struct Backdrop: Codable {
+    let aspectRatio: Double
+    let filePath: String
+    let height: Int
+    let width: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case aspectRatio = "aspect_ratio"
+        case filePath = "file_path"
+        case height
+        case width
+    }
+}
