f4e4f211273502a9385521c098c5915094a52ca8
TonyJem <a.jemanov@gmail.com>
2022-05-02 22:27:58 +0300

    Update BigScreenShotCollectionViewCell.swift

--- FilmsApp/FilmsApp/Models/BigScreenShotCollectionViewCell.swift
+++ FilmsApp/FilmsApp/Models/BigScreenShotCollectionViewCell.swift
@@ -10,6 +10,9 @@ class BigScreenShotCollectionViewCell: UICollectionViewCell {
             let imagePath = Core.tempStorage.screenshots[selectedId]
             guard let url = URL(string: Constants.urlBase + imagePath) else { return }
             Core.urlService.getSetPosters(withURL: url, imageView: screenshotImageView)
+            
+            screenshotImageView.layer.borderColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0).cgColor
+            screenshotImageView.layer.borderWidth = 2
         }
     }
 }
