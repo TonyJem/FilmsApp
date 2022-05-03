1c46cceff9aa557fed0f81c481cc924a12163850
TonyJem <a.jemanov@gmail.com>
2022-05-03 21:33:19 +0300

    Update SingleScreenShotViewController.swift

--- FilmsApp/FilmsApp/Controllers/SingleScreenShotViewController.swift
+++ FilmsApp/FilmsApp/Controllers/SingleScreenShotViewController.swift
@@ -49,20 +49,20 @@ extension SingleScreenShotViewController: UICollectionViewDataSource {
 
 // MARK: - CollectionView DelegateFlowLayout
 extension SingleScreenShotViewController: UICollectionViewDelegateFlowLayout {
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-
+        
         return 10
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-
+        
         let frameSize = collectionView.frame.size
         return CGSize(width: frameSize.width - 10, height: frameSize.height)
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-
+        
         return UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
     }
 }
