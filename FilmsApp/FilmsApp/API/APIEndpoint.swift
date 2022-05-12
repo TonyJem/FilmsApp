f0f3d199755f9f9c6aac0f880ca07b9779bb48b8
TonyJem <a.jemanov@gmail.com>
2022-05-12 21:12:46 +0300

    WIP

--- FilmsApp/FilmsApp/API/APIEndpoint.swift
+++ FilmsApp/FilmsApp/API/APIEndpoint.swift
@@ -1,16 +1,18 @@
 import Foundation
 
 enum APIEndpoint {
-    case imagesPaths
+    case images
     
     var url: URL? {
         switch self {
-        case .imagesPaths:
-            return makeURL(endpoint: "endpoint")
+        case .images:
+            return makeURL(endpoint: "/images?api_key=")
         }
     }
 }
 
+
+//let idString = String(id)
 
 // https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=<<api_key>>
 private extension APIEndpoint {
@@ -23,7 +25,8 @@ private extension APIEndpoint {
         "a7e7734795f96137ff3605cc57a01ddb"
     }
     
-    func makeURL(endpoint: String) -> URL? {
-        return URL(string: baseURL + endpoint)
+    func makeURL(for id: Int, with endpoint: String) -> URL? {
+        let idString = String(id)
+        return URL(string: baseURL + idString + endpoint + apiKey)
     }
 }
